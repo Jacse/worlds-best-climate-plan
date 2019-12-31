150f1c655679282fce6bebffb7f91a4f6c4d1faf
Jacob Lauritzen <jacsebl@hotmail.com>
2019-12-31 10:26:28 +0100

    Add long read text

--- src/i18n/da.js
+++ src/i18n/da.js
@@ -11,11 +11,74 @@ export default {
   moreAboutThePlan: 'Mere om planen',
   behindThePlan: 'Bag planen',
   plan: `
-  #### Header
-  Paragraph
-  #### Header 2
-  paragraph 2
-  still p 2
+Ligesom alle os andre har du nok lidt klimaangst. Du ved, at hvis vi ikke gør noget drastisk snart, så bliver det slemt. Men. Du vil stadigvæk gerne bruge din bil i fred, og demonstrationer med Greta Thunberg er måske ikke helt din kop te.
+
+Du kunne godt tænke dig, at klimaet fikser sig selv, og at du ikke behøver tænke over det. Det, du godt kunne tænke dig, er en slags “doven revolution”.
+
+Vi holder med dig. Der er en løsning, der er så simpel og så kedelig, at politikere ikke gider at snakke om det. Den går ud på, at det skal koste noget at udlede drivhusgasser, men uden at den gennemsnitlige dansker skal have penge op af lommen eller at det skader danske arbejdspladser.
+
+Vi ved allerede hvad du tænker, og du kan bare tage det helt roligt. Vi lover, at dette forslag ikke er grebet ud af luften.
+
+Det er faktisk så gennemtænkt, at 27 Nobelprismodtagere støtter det. Det samme gør 3.500 økonomer og flere hundrede virksomheder.
+
+Og det tager kun 5 minutter at få det til at ske, så det passer lige til reklamepausen. Så er du done. Det er den grønne omstilling fra din sofa. Det er verdens bedste klimaplan.
+
+## Lad forureneren betale
+
+Hvis du smider skrald ud af vinduet, ender du nok med en klækkelig bøde. Det gør du fordi det er super irriterende for os andre at skulle tage os af det.
+
+Når vi derimod forurener med drivhusgasser, som forårsager klodens globale opvarmning, så er det helt cool og ganske gratis. Forestil dig to virksomheder der begge sælger T-shirts. Den første fabrik er drevet på elektricitet fra vindmøller. Den anden på el fra olie. Den oliedrevne fabrik forurener langt mere end den vindmølledrevne. Alligevel er der ingen, der kommer efter dem med den klækkelige bøde, som du fik, da du kastede skraldeposen ud af vinduet.
+
+Det skal vi selvfølgelig have sat en stopper for. Og løsningen er simpel: forurenere skal betale for deres eget skrald, også hvis skraldet hedder drivhusgasser.
+
+Verdensbanken, OECD, Den Internationale Valutafond, Nationalbanken, Klimarådet og Det Økonomiske Råd er også enige: den hurtigste og billigste måde at komme af med vores udledninger er at sætte en pris på dem.
+
+Den gode nyhed er, at vi allerede ved, hvor næsten alle drivhusgasser kommer fra. når vi brænder fossile brændsler (olie, gas, kul), når vi opdrætter køer, og når vi producerer gødning og producerer cement. Derfor ved vi også, hvor regningen skal sendes hen.
+
+## Et bidrag der ikke koster noget for de fleste
+
+Lad os være ærlige. Hvis den oliedrevne T-shirt-producent pludselig skal betale for den CO2, de smider ud i atmosfæren… Ja, så smider virksomheden nok den nye, irriterende omkostning på en eller anden måde videre til deres kunder, som i sidste ende sender den videre til dig.
+
+Ligesom en afgift på sukker gør fredagssnold dyrere vil en afgift på CO2 gøre klimabelastende varer dyrere. Det betyder, at når du smutter forbi Netto, så vil varernes klimabelastning afspejles i prisen. Du vil derfor ikke længere behøve at tjekke om agurken komme fra Samsø eller Samoa.
+
+Når slik bliver dyrere, spiser vi mindre slik. Når klimabelastende varer bliver dyrere, køber vi færre klimabelastende varer og flere klimavenlige varer.
+
+For at sikre at Hr. og Fru Jensen ikke betaler mere for deres livsstil end de gør i dag, gør vi brug af et snedigt trick: Alle penge, der er blevet opkrævet fra klimasyndere, bliver givet tilbage ligeligt til borgerne. Ja, du læste rigtigt.
+
+
+Det betyder, at hvis du køber færre klimabelastende ting end den gennemsnitlige dansker, så ender du faktisk med flere penge i lommen. Selv hvis du er milliardær men holder dig fra at flyve, bor i en lille lejlighed og cykler på arbejde, ender du med at være rigere, end du var før.
+
+På den måde belønner vi de klimavenlige med penge fra de klimafjendtlige. Faktisk vil de fleste husstande få flere penge end de vil skulle bruge på prisstigningen af forurenende varer.
+
+
+## Lad os lade være med at forbyde de sjove sager
+
+Lad os undgå, at staten tager vores yndlingslegetøj væk fra os. De røde bøffer, det billige Ryanair-fly til Tenerife, den kæmpe Audi og Rema1000-plastikposen. Disse skal stadigvæk være tilgængelige - men lad os sikre, at forureningen er betalt for.
+
+Prisen for alle disse ting vil afhænge af, hvad det koster at forurene. I dag er prisen på CO2 allerede ca. 200 kroner per ton. Men vi ved, at det koster hele 7.000 kroner at fjerne CO2 direkte fra luften med maskiner. Heldigvis behøver prisen per CO2-ton ikke være lige så høj for at se de ønskede resultater, selvom der er nogen uenighed om den optimale pris. Ifølge De Økonomiske Råd burde den ligge omkring 563 kroner per ton CO2. Forskere fra Stanford peger dog på, at prisen burde være 1.600 kroner. Her kan du se, hvordan forskellige forurenings-priser vil påvirke prisen for nogle almindelige varer.
+
+Prisen på forurening behøver ikke starte på det højeste niveau. I stedet skal den stige støt. Det gør at alle kan ændre deres adfærd med forståelse for, hvad fremtiden bringer. Selv de største klimasyndere bør have lidt tid til at blive klimavenlige.
+
+At have en pris på forurening er faktisk et meget billigere tiltag end når politikere vælger specifikke forbud eller rabatter. Når man forbyder ting, ender det ofte med, at det kun er folk, som bor på Strandvejen, der har råd til alternativerne, der så er tilgængelige. Tænk blot på hvem der ville kunne købe en bil, hvis benzinbiler blev forbudt. Hertil kommer, at politikerne er elendige gamblere. Når de en gang imellem vælger at give ekstra støtte til enkelte varer, ender de ofte med at give penge til ting, der var cool for alt for mange år siden.
+Lad os lade være med det.
+
+## En god deal for virksomheder
+
+Den bedste klimaplan er no bueno hvis jobbene smutter til Polen eller Kina. Den gode nyhed er, at danske virksomheder kommer til at elske den bedste klimaplan. Ikke blot fordi det er den billigste løsning, men også fordi den gør det nemt for dem at lave de rigtige investeringer i dag, fordi de ved, hvordan priserne kommer til at ændre sig i fremtiden. Virksomheder elsker forudsigelighed.
+
+Arbejdspladserne flytter sandsynligvis ikke til Gdansk eller Shenzhen: Paris-aftalen og EUs ambitioner kommer til at tvinge alle lande til at reducere deres udledninger til 0 inden 2050. Det betyder, at andre lande faktisk heller ikke er interesseret i, at der kommer forurenende forretninger til deres land. Ligegyldigt hvor de flytter hen bliver virksomheder tvunget til at blive klimaneutrale lige om hjørnet.
+
+Nogle industrier kan dog blive udfordret på kort sigt. Der er heldigvis mange muligheder for at sørge for, at de ikke bliver mindre konkurrencedygtige af den bedste klimaplan.
+
+En fornuftig måde vil være at justere lidt på prisen for at forurene for nogle sektorer, der på grund af konkurrencen vil have mest at tabe. En anden måde kunne være at give penge til virksomheder der sælger mange varer til lande, hvor klimaet endnu ikke er deres største bekymring (en bundfradrag), så deres varer fortsat kan sælges i andre lande uden problemer.
+
+Sidst men ikke mindst: hvis vores egne virksomheder bliver grønnere, betyder det, at de bliver konkurrencedygtige i en verden, der fremover gerne skal købe flere klimavenlige varer. Det kan altså sætte skub i eksporten.
+
+## Støt verdens bedste klimaplan
+
+Nu er du nået til det sted, hvor du skal få fingeren ud så vi kan sætte den bedste klimaplan i gang! Saml al din klimaangst, al din like-iver, følg dit nytårsforsæt og skriv under på borgerforslaget.
+
+Når du er færdig skal du lige komme tilbage og dele den med din bedstemor, din klimaskeptiske onkel og din hippie-veninde. Det er nu, det gælder. Lad os få den bedste klimaplan i gang så vi alle kan læne os tilbage og fokusere på de mange andre ting, vi hellere vil bruge vores tid på.
   `,
   frontpage: {
     explainedBriefly: 'Planen kort fortalt',
