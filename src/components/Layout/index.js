ef6cd1ee529198b632acc67eb64c59ff9a957eb9
Jacob Lauritzen <jacsebl@hotmail.com>
2020-01-01 15:30:56 +0100

    Close #25

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -42,6 +42,11 @@ const Layout = ({ children }) => (
         <p className={styles.copyright}>&copy; Verdens Bedste Klimaplan 2020</p>
       </div>
     </div>
+    <script
+      key="cookie-bar"
+      type="text/javascript"
+      src="https://cdn.jsdelivr.net/npm/cookie-bar/cookiebar-latest.min.js?tracking=1&thirdparty=1&always=1&scrolling=1"
+    ></script>
   </div>
 );
 
