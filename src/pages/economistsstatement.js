eb5e0f1aa21254b8f8e28070bdfbc46cc4be92b4
martincollignon <danskcollignon@gmail.com>
2020-01-07 13:54:12 +0100

    Update economistsstatement.js

--- src/pages/economistsstatement.js
+++ src/pages/economistsstatement.js
@@ -5,13 +5,11 @@ import text from '../i18n/da';
 import styles from './index.module.css';
 import Separator from '../components/Separator';
 import SEO from '../components/SEO';
-import Button from '../components/Button';
-import { ModalContext } from '../components/SupportModal';
 
 const EconomistsStatement = () => {
   return (
     <Layout>
-      <SEO title="Mere om planen" description="" />
+      <SEO title="Økonomernes udsagn" description="" />
       <section className={[styles.top, 'green', 'separator-bottom'].join(' ')}>
         <div className="container-large">
           <h1>{text.economistsStatementHeader}</h1>
@@ -20,17 +18,9 @@ const EconomistsStatement = () => {
       <section>
         <Separator variant={1} />
         <div className="container-small">
-          <ReactMarkdown source={text.economistsStatement} />
+          <ReactMarkdown source={text.economistsStatementText} />
           <div style={{ textAlign: 'center' }}>
-            <ModalContext.Consumer>
-              {openModal => (
-                <div className={styles.ctas}>
-                  <Button onClick={openModal} large className="btn-green">
-                    {text.supportAsPerson}
-                  </Button>
-                </div>
-              )}
-            </ModalContext.Consumer>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf-wtAyJBnTpPggT2-OSzW64FFnhG20ajBHAFQZwAEjJGC-HA/viewform?embedded=true" width="640" height="1187" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
           </div>
         </div>
       </section>
