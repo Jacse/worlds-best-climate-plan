8f845184e33ba5cbc94bd6071571c1af9af1b3ef
Jacob Lauritzen <jacsebl@hotmail.com>
2020-01-19 17:32:11 +0100

    Fix #37

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Menu from '../Menu';
 import Footer from '../Footer';
 import SupportModalProvider from '../../components/SupportModal';
 
 const Layout = ({ children }) => (
   <SupportModalProvider>
+    <Helmet>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+    </Helmet>
     <Menu />
     {children}
     <Footer />
