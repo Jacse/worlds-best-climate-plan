c0194800c808f817b88ff303f9309cb93de53190
Jacob Lauritzen <jacsebl@hotmail.com>
2020-01-19 17:17:03 +0100

    Lint

--- src/pages/economistsstatement.js
+++ src/pages/economistsstatement.js
@@ -20,7 +20,16 @@ const EconomistsStatement = () => {
         <div className="container-small">
           <ReactMarkdown source={text.economistsStatementText} />
           <div style={{ textAlign: 'center' }}>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf-wtAyJBnTpPggT2-OSzW64FFnhG20ajBHAFQZwAEjJGC-HA/viewform?embedded=true" width="640" height="1187" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSf-wtAyJBnTpPggT2-OSzW64FFnhG20ajBHAFQZwAEjJGC-HA/viewform?embedded=true"
+              width="640"
+              height="1200"
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
+            >
+              Loading…
+            </iframe>
           </div>
         </div>
       </section>
