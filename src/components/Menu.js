75a37b1402a658a35e6e28c2d3442e3783202daa
Jacob Lauritzen <jacsebl@hotmail.com>
2020-02-24 15:41:01 +0100

    Remove burger menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,83 +9,7 @@ const pages = [
   ['/about-us', 'Bag planen'],
 ];
 
-const MenuItems = ({ large = false }) => (
-  <React.Fragment>
-    {pages.map(([href, title]) => (
-      <Link
-        className={classNames(
-          'cursor-pointer m-6 text-sand-100 font-bold',
-          large ? 'hidden md:block' : 'block md:hidden'
-        )}
-        to={href}
-        key={href}
-      >
-        {title}
-      </Link>
-    ))}
-  </React.Fragment>
-);
-
-const BurgerButton = ({ setsideBarActive }) => (
-  <div
-    className="relative cursor-pointer md:hidden ml-8"
-    style={{
-      width: 26,
-      height: 22,
-    }}
-    onClick={() => setsideBarActive(true)}
-  >
-    <span>
-      <span
-        className="absolute inset-x-0 bg-white"
-        style={{ height: '20%', top: 0 }}
-      ></span>
-      <span
-        className="absolute inset-x-0 bg-white"
-        style={{ height: '20%', top: '40%' }}
-      ></span>
-      <span
-        className="absolute inset-x-0 bg-white"
-        style={{ height: '20%', top: '80%' }}
-      ></span>
-    </span>
-  </div>
-);
-
-const CloseButton = ({ setsideBarActive }) => (
-  <div
-    className="absolute p-4 top-0 right-0"
-    style={{
-      top: '1rem',
-      right: '1rem',
-      width: 26,
-      height: 26,
-    }}
-    onClick={() => setsideBarActive(false)}
-  >
-    <span
-      className="absolute bg-white cursor-pointer"
-      style={{
-        width: 3,
-        height: 26,
-        transformOrigin: 'center',
-        transform: 'rotate(45deg)',
-      }}
-    ></span>
-    <span
-      className="absolute bg-white cursor-pointer"
-      style={{
-        width: 3,
-        height: 26,
-        transformOrigin: 'center',
-        transform: 'rotate(-45deg)',
-      }}
-    ></span>
-  </div>
-);
-
 const Menu = ({ transparent = false }) => {
-  const [sideBarActive, setsideBarActive] = useState(false);
   const [isSticky, setSticky] = useState(false);
 
   const handleScroll = () => {
@@ -110,31 +34,28 @@ const Menu = ({ transparent = false }) => {
           }
         )}
       >
-        <BurgerButton setsideBarActive={setsideBarActive} />
         <Link to="/">
           <img
             src="/logo_white.png"
             alt=""
-            className="h-12 md:h-12 my-2 mx-8"
+            className="h-12 md:h-12 m-4 md:mx-8"
           />
         </Link>
-        <MenuItems large />
-        <Button secondary className="mr-8 ml-auto">
+        {pages.map(([href, title]) => (
+          <Link
+            className="cursor-pointer m-6 text-sand-100 font-bold hidden md:block"
+            to={href}
+            key={href}
+          >
+            {title}
+          </Link>
+        ))}
+        <Button secondary className="mr-4 md:mr-8 ml-auto">
           Skriv under 🌱
         </Button>
       </div>
-      <div
-        className="fixed inset-0 bg-green-800 z-20 p-4 text-center"
-        style={{
-          transform: `translate3d(${sideBarActive ? 0 : '-100%'}, 0, 0)`,
-          transition: 'transform 0.2s ease-in-out',
-        }}
-      >
-        <CloseButton setsideBarActive={setsideBarActive} />
-        <MenuItems />
-      </div>
       {/* filler */}
-      {!transparent && <div className="h-16 md:h-20 w-full"></div>}
+      {!transparent && <div className="h-20 w-full"></div>}
     </div>
   );
 };
