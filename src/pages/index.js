8172068138f2054532d5d96259165bf2bb97d6f6
Jacob Lauritzen <jacsebl@hotmail.com>
2020-02-25 12:42:51 +0100

    Fix index paddings and margins

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const Index = () => {
       <SEO title="Verdens Bedste klimaplan" description="" />
       {/* Hero and letter */}
       <section
-        className="lg:pt-20 relative mb-64"
+        className="pt-20 relative mb-64"
         style={{
           background: 'url(../../background.jpg)',
           backgroundSize: 'cover',
@@ -25,25 +25,15 @@ const Index = () => {
               'linear-gradient(rgba(1, 27, 26, 0.2), rgba(1, 27, 26, 1))',
           }}
         ></div>
-        <div className="relative mx-auto p-24">
-          <h1 className="text-center text-2xl md:text-4xl font-bold text-sand-100 leading-none">
+        <div className="relative mx-auto md:py-24 p-8">
+          <h1 className="text-center text-xl sm:text-2xl md:text-4xl font-bold text-sand-100 leading-none">
             Støt en ny dansk klimaskat
           </h1>
-          <p className="text-center text-xl my-8 text-sand-100">
+          <p className="max-w-xl mx-auto text-center md:text-xl my-8 text-sand-100">
             Danmark skal have en afgift på varer der ødelægger klimaet.
-            <br />
             Forureneren betaler og pengene gives tilbage til borgerne.
           </p>
-          <Button
-            onClick={() =>
-              document
-                .getElementById('sign')
-                .scrollIntoView({ behavior: 'smooth' })
-            }
-            className="block mx-auto"
-          >
-            Skriv under nu ✍️
-          </Button>
+          <Button className="block mx-auto">Skriv under nu ✍️</Button>
         </div>
         {/* Letter */}
         <div className="transform -mt-64 translate-y-64">
@@ -51,10 +41,15 @@ const Index = () => {
         </div>
       </section>
       {/* Sådan virker det */}
-      <section className="mx-auto max-w-2xl px-6 pt-12">
+      <section className="mx-auto max-w-2xl px-8 pt-12">
         <HowItWorks />
       </section>
-      <Form />
+      {/* Support form */}
+      <section className="mx-auto max-w-4xl p-8 md:p-20 bg-sand-100 mb-12">
+        <div className="mx-auto max-w-lg">
+          <Form />
+        </div>
+      </section>
     </Layout>
   );
 };
