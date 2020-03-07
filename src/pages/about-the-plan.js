15d997ede7aa2304ca7d8ebcf3d13a53d174af4b
Jacob Lauritzen <jacsebl@hotmail.com>
2020-03-07 16:37:03 +0100

    Fix #73

--- src/pages/about-the-plan.js
+++ src/pages/about-the-plan.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Collapse, { Panel } from 'rc-collapse';
+import { FaChevronCircleDown, FaChevronCircleUp } from 'react-icons/fa';
 import Layout from '../components/Layout';
 import FootNoteContainer, {
   FootNoteItem as FootNote,
@@ -8,6 +10,81 @@ import SEO from '../components/SEO';
 const P = ({ children }) => (
   <p className="my-4 leading-loose text-gray-800">{children}</p>
 );
+
+const faq = [
+  [
+    'Hvorfor skal Danmark have verdens bedste klimaplan?',
+    'Danmarks politikere er blevet enige: Danmark skal reducere sine udledninger med 70 pct. ift. niveauet i 1990. Det kan sagtens nås, selvom det er ambitiøst. Ja, faktisk meget ambitiøst. Det eneste andet land, der er så ambitiøs er Sverige. \n\n Hvis man kigger på verdens samlede drivhusgasudledninger, så udgør Danmark kun en brøkdel. I 2014 udledte Danmark 0.27% pct af verdens samlede drivhusgasudledninger. Men hvorfor skal vi så gøre noget overhovedet? \n\n Her kommer den ubekvemme sandhed: Hvis vi kigger på vores nuværende klimaaftryk per indbygger, så er Danmark et af de slemmeste lande - og det er endnu værre, hvis vi kigger på al den historiske udledning - altså den forurening, som Danmark har været skyld i siden det industrielle gennembrud. Det giver os et ansvar for at gøre det bedre. \n\n Vi er en af de slemmeste drenge i klassen, men samtidig har vi kæmpe ambitioner om at blive bedre, og det giver os en unik mulighed. \n\n Danmark, hvis vi vælger den bedste klimaplan, kan vise hele omverdenen, at en grøn omstilling kan lade sig gøre - og dermed inspirere alle andre lande til at gøre det samme.',
+  ],
+  [
+    'Er en pris på forurening nok til at give os 70 pct. CO2-reduktion i 2030?',
+    'Nej, fordi vi har mægtig travlt. Men det kan få os langt og bedst muligt hen ad vejen. Derfor skal politikerne også investere i forskning, infrastruktur og incitamenter for at få de sidste procenter med.',
+  ],
+  [
+    'Hvad er ulemperne? Det lyder for godt til at være sandt.',
+    'Desværre er afgiften ikke nok i sig selv til at nå 70%. Derfor skal politikerne også træffe hårde beslutninger om fx. bilfrizoner, ladeinfrastruktur, bygningsreglement, olieudvinding og forskning',
+  ],
+  [
+    'Skal løsningen ikke bruges i hele EU eller hele verden før den er effektiv?',
+    'Det vil være den ideelle løsning, hvis verdens bedste klimaplan blev implementeret i hele verden eller i hele EU. Uanset hvad der sker udenfor vores grænser, er økonomerne stadigvæk overbeviste om, at det er bedst for Danmark at bruge planen, fordi det er den billigste og bedste vej for at nå vores mål, og så kan vi også vise vejen for de andre lande.',
+  ],
+  [
+    'Skal man regne, hvor meget CO2 der er i ethvert produkt?',
+    'Nope. Her består tricket i at lægge afgiften så tæt på forureningskilden som muligt; på fossile brændsler, drøvtyggere og materialerne til forurenende produktion såsom cement. Når prisen på CO2 bliver indregnet i disse produkter fra starten, vil afgiften rykke med fra produkt til produkt, der benytter sig af fx olie, kul eller cement.',
+  ],
+  [
+    'Skal man fjerne alle eksisterende CO2-afgifter?',
+    'Det behøver man faktisk ikke! Mange af vores eksisterende klimaafgifter i dag bruges til at finansiere vores velfærdsstat, og at fjerne dem vil kræve, at man finder nye indtægtskilder. Derfor er det nemmeste blot at lægge dem ovenpå de eksisterende afgifter - og lade politikerne tænke, hvordan de skal finansiere velfærden i fremtiden.',
+  ],
+  [
+    'Skal industrier, der allerede er dækket af ETS-kvotesystemet også betale afgiften?',
+    'Det skal de, da ETS-kvoterne er for billige til, at de kan støtte 70 pct. målet. Man kan eventuelt godt refundere det der svarer til ETS-prisen til de pågældende industrier.',
+  ],
+  [
+    'Skal alle pengene gives tilbage til borgerne?',
+    'Ideelt, jep! Pengene fordeles fuldstændig ligeligt mellem alle borgere, så uanset om du er millionær eller på kontanthjælp, får du det samme beløb ind på kontoen. For de fleste vil dette beløb mere end dække omkostningerne ved højere priser på sorte varer, og jo grønnere du lever, jo gladere vil din pengepung blive. I andre lande har man valgt at bruge en andel på velfærd eller forskning - det er dog ikke hvad økonomerne anbefaler.',
+  ],
+  [
+    'Smutter alle jobs til udlandet?',
+    'Det er meget usandsynligt. For det første er mange af de andre lande også ved at blive ambitiøse (og er derfor ikke interesserede i at få klimasyndere hos dem). Fordi EU har planer om en klimatold, så vil ikke-EU lande også være ambitiøse. Der findes også muligheder for at forbedre vores konkurrenceevne. Sidst men ikke mindst betyder verdens bedste klimaplan, at vi alle får travlt med den grønne omstilling. Der kommer til at opstå mange nye muligheder, fordi grønne produkter bliver mere konkurrencedygtige.',
+  ],
+  [
+    'Er der andre lande, der har gjort det før?',
+    'British Columbia i Canada har gjort det med succes - og det bliver nu udrullet i hele Canada. Det er på dagsorden i flere andre lande.',
+  ],
+  [
+    'Hvad siger industrien?',
+    'Nogle store danske virksomheder støtter en pris på forurening. Det gælder eksempelvis Nordea, Danfoss, Vestas, og Ørsted. Dansk Industri og Dansk Erhverv har generelt været imod “dumme afgifter”. Så er det heldigt, at verdens bedste klimaplan er super smart! Fordi den er billigere samfundsøkonomisk og mere forudsigelig end alternativerne, er det nærmest en selvfølge, at virksomhederne overordnet kommer til at elske planen. Nogle kommer til at brokke sig (især klimasyndere), men det er mere fordi nogle af deres forretninger ikke rigtig kan være bæredygtig - og de kan godt se at det bliver svært at være klimaneutrale i 2050. Men selve Aalborg Portland og Arla burde se fidusen i det - så kan de sælge mere af henholdsvis Futurecem og plantedrik',
+  ],
+  [
+    'Hvor meget skal det koste at udlede CO2?',
+    'For at vi skal af med vores udledninger, skal prisen være tilstrækkelig høj. Klimarådet nævnte i 2018 en pris på over 800 kroner per ton udledt CO2, men det var før målet om 70 pct. blev til. Det er sandsynligt, at prisen vil være mellem 1.000 og 1.500 kroner i 2030.',
+  ],
+  [
+    'Hvordan finansierer vi velfærdsstaten hvis afgifterne fra fossile brændsler ikke findes længere?',
+    'Hvis vi antager, at vi rent faktisk gerne vil være klimaneutrale i 2050, så kommer vi ikke til at bruge fossile brændsler, og derfor skal vi uanset hvad finde nye indtægtskilder. Det har sådan set ikke rigtigt noget med verdens bedste klimaplan at gøre.',
+  ],
+  [
+    'Betyder det, at jeg ikke kan flyve længere?',
+    'Nej. Det betyder simpelthen bare, at du betaler den reelle pris for din flyrejse. En smuttur til Paris vil derfor blive dyrere end i dag, men hvis du ønsker at prioritere det, vil der stadig være plads i budgettet til den årlige ferie for almindelige familier. Samtidig vil en pris på CO2 skubbe flyselskaberne til at få fart på udviklingen af klimavenlige brændstoffer og flyvemaskiner, som vil gøre grønne rejser mulige i fremtiden.',
+  ],
+  [
+    'Kan man ikke bare forbyde tingene i stedet for?',
+    'Det kunne man, men så er det mange ting man skal forbyde, og når man forbyder ting går det hårdere ud over de svageste, der ofte ikke har råd til alternativer. Med verdens bedste klimaplan vil varens reelle pris blive brugt - og så bestemmer man selv om man synes en vare er det værd.',
+  ],
+  [
+    'Kunne man ikke bare ikke sætte et klimamærke på alt og lade folk selv bestemme om de vil købe dem?',
+    'Det vil være en god start, men det er meget usandsynligt, at det vil gøre en forskel. Der er en grund til, at man nu sætter afgiften på cigaretter op, selvom der har være grimme billeder på indpakningen i et par år.',
+  ],
+  [
+    'Er det ikke bare en skjult måde at lave borgerløn på?',
+    'At give pengene tilbage gennem den grønne check er kun en metode at gøre det på. Man kunne også vælge at reducere andre afgifter, eller at hæve personfradraget. Beløbene vil dog ikke være særlige store for de fleste, så det er usandsynligt, at det vil påvirke borgernes bankkonto på en måde, der vil gøre at de ikke vil i arbejde.',
+  ],
+  [
+    'Kan vi ikke bare støtte virksomhederne gennem den grønne omstilling i stedet?',
+    'Det kunne man, men det er en dårligere løsning. Det er det fordi pengene kommer fra den fælles kasse (altså dine skattepenge), og fordi det er rigtig svært at gøre nok for at nå de 70 pct. Hvert år ville politikerne skulle beregne, hvor meget CO2 man har sparet, og derefter lave nye tiltag i stedet for blot at skrue op for afgiften. Det er uoverskueligt - og på ingen måde ambitiøst nok.',
+  ],
+];
 
 const ThePlan = () => {
   return (
@@ -451,6 +528,34 @@ const ThePlan = () => {
             konkurrenceevne, eller at den tunge ende vender nedad.
           </P>
         </FootNoteContainer>
+        <section>
+          <div className="w-full max-w-2xl mx-auto my-8">
+            <h1 className="text-2xl my-6 font-bold">Spørgsmål og svar</h1>
+            <Collapse
+              accordion
+              className="bg-transparent border-0 text-green-900"
+              expandIcon={({ isActive }) =>
+                isActive ? (
+                  <FaChevronCircleUp className="mr-4 flex-none text-green-500 text-2xl" />
+                ) : (
+                  <FaChevronCircleDown className="mr-4 flex-none text-green-500 text-2xl" />
+                )
+              }
+            >
+              {faq.map(([question, answer]) => (
+                <Panel
+                  key={question}
+                  header={question}
+                  headerClass="flex items-center py-4 cursor-pointer"
+                >
+                  {answer.split('\n').map((paragraph, i) => (
+                    <P key={i}>{paragraph}</P>
+                  ))}
+                </Panel>
+              ))}
+            </Collapse>
+          </div>
+        </section>
       </div>
     </Layout>
   );
